3a97c8a864b11040af63681ffef51f8f18757ad6
Aline Souza <ds.alinesouza@gmail.com>
2021-09-04 20:52:58 -0300

    cria classe usuario

---
+++ src/main/java/br/edu/infnet/domain/model/Usuario.java
@@ -0,0 +1,99 @@
+package br.edu.infnet.domain.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nomeCompleto;
+    private String numeroTelefone;
+    private String email;
+    private String senha;
+    private String cpf;
+    private String cnpj;
+    private String empresa;
+    private Character type;
+    public static final char ADMIN = 'A';
+    public static final char CANDIDATO = 'C';
+    public static final char EMPRESA = 'E';
+
+    public Usuario(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getNumeroTelefone() {
+        return numeroTelefone;
+    }
+
+    public void setNumeroTelefone(String numeroTelefone) {
+        this.numeroTelefone = numeroTelefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public Character getType() {
+        return type;
+    }
+
+    public void setType(Character type) {
+        this.type = type;
+    }
+}
