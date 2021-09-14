ef0442317be60de3a3b620bf782ac159a27ec7d7
Aline Souza <ds.alinesouza@gmail.com>
2021-09-13 23:25:16 -0300

    acrescenta componentScan

--- src/main/java/br/edu/infnet/VenturaHrUsuarioApplication.java
+++ src/main/java/br/edu/infnet/VenturaHrUsuarioApplication.java
@@ -1,9 +1,16 @@
 package br.edu.infnet;
 
+import br.edu.infnet.domain.controller.UsuarioController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = {UsuarioController.class})
+@EntityScan(basePackages = {"br.edu.infnet.domain.model"})
+@EnableJpaRepositories(basePackages = {"br.edu.infnet.domain.repository"})
 public class VenturaHrUsuarioApplication {
 
     public static void main(String[] args) {
